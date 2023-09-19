56a65344f6bd7f8fefb1a5358bd92b01480f5603
Antonin Geslin Dugué <latouillette@MacBook-Air-de-Antonin.local>
2023-09-19 22:01:43 +0200

    ajout de features css aux taches qui sont ajoutées pour chaque utilisateur

--- index.php
+++ index.php
@@ -31,20 +31,17 @@
         if (isset($_SESSION['login'])) {
             echo '<div class="add_task">
                 <input id="myInput" type="text" name="task" placeholder="Task">
-                <button type="submit" placeholder="Search">Add Task</button>
+                <button type="submit" placeholder="Search"><?xml version="1.0" encoding="utf-8"?><!-- Uploaded to: SVG Repo, www.svgrepo.com, Generator: SVG Repo Mixer Tools -->
+                    <svg width="64" height="64" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <circle cx="12" cy="12" r="10" stroke="#1C274C" stroke-width="1.5"/>
+                    <path d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15" stroke="#1C274C" stroke-width="1.5" stroke-linecap="round"/>
+                    </svg>
+                </button>
             </div>';
         }
     ?>
 
     <div class="taskList">
-        <div class="task">
-                <h1>Task 2</h1>
-                <input type="checkbox" name="check"/>
-        </div>
-        <div class="task">
-                <h1>Task 2</h1>
-                <input type="checkbox" name="check"/>
-        </div>
     </div>
 </body>
 </html>

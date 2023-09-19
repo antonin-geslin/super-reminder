0e49eb8635b03400415e9259d5b4573fd480213d
Antonin Geslin Dugué <latouillette@MacBook-Air-de-Antonin.local>
2023-09-19 15:51:04 +0200

    ajout de la feature qui permet d'afficher uniquement les tâches de l'user connecté

--- server/get_tasks.php
+++ server/get_tasks.php
@@ -1,13 +1,26 @@
 <?php 
-    try {
-        $bdd = new PDO('mysql:host=localhost;dbname=superreminder;charset=utf8', 'root', 'root');
-        $requete = $bdd->prepare("SELECT * FROM tasks");
-        $requete->execute();
-    
-        $tasks = $requete->fetchAll(PDO::FETCH_ASSOC);
-    
-        echo json_encode($tasks);
-    } catch (PDOException $e) {
-        echo $e->getMessage();
+
+    session_start();
+    if (isset($_SESSION['login'])) {
+        try {
+            $bdd = new PDO('mysql:host=localhost;dbname=superreminder;charset=utf8', 'root', 'root');
+
+            $requeteUser = $bdd->prepare("SELECT id FROM users WHERE login = :login");
+            $requeteUser->bindParam(':login', $_SESSION['login']);
+            $requeteUser->execute();
+            
+            $user = $requeteUser->fetch(PDO::FETCH_ASSOC);
+            $id = $user['id'];
+
+
+            $requete = $bdd->prepare("SELECT * FROM tasks where id_user = :id");
+            $requete->bindParam(':id', $id);
+            $requete->execute();
+            $tasks = $requete->fetchAll(PDO::FETCH_ASSOC);
+        
+            echo json_encode($tasks);
+        } catch (PDOException $e) {
+            echo $e->getMessage();
+        }
     }
 ?>
